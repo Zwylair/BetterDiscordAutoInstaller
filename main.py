75b61e2a9fd11e6723d9447da3a74291252f159b
Zwylair <82669735+Zwylair@users.noreply.github.com>
2025-02-06 16:36:18 +0200

    add bd version checking

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import time
-from funcs import *  # Import functions from funcs.py
+from funcs import *  # Import functions
 import funcs  # For editing the variables
 
 def main():
-    try:
-        load_settings()
-    except Exception as e:
-        logger.error(str(e))
-        sys.exit(1)
+    load_settings()
 
     logger.info(f'BetterDiscordAutoInstaller v{BDAI_SCRIPT_VERSION}')
 
@@ -15,49 +11,62 @@ def main():
     if check_for_updates() and not funcs.DISABLE_BDAI_AUTOUPDATE:
         run_updater()
 
-    logger.info("Killing any running Discord processes...")
-    kill_discord()
-    time.sleep(2)
+    if not is_discord_running():
+        logger.info("Discord is not running so I can't make sure it's up to date. You should have Discord open when starting BDAI.")
+        logger.info("Exiting in 3 seconds...")
+        time.sleep(3)
+        sys.exit(0)
 
-    logger.info('Checking if Discord is installed and updated...')
+    logger.info("Retrieving Discord installation folder...")
     funcs.DISCORD_PARENT_PATH = find_discord_path()
     if not funcs.DISCORD_PARENT_PATH:
         logger.error("No valid Discord installation found.")
+
         funcs.DISCORD_PARENT_PATH = input("Enter the path to your Discord installation: ").strip()
         if not os.path.exists(funcs.DISCORD_PARENT_PATH):
             logger.error(f"Invalid path provided: {funcs.DISCORD_PARENT_PATH}")
             sys.exit(1)
 
-    try:
-        discord_core_folder = get_latest_installed_discord_folder_name(funcs.DISCORD_PARENT_PATH)
-        discord_path = os.path.join(funcs.DISCORD_PARENT_PATH, discord_core_folder)
+    discord_core_folder = get_latest_installed_discord_folder_name(funcs.DISCORD_PARENT_PATH)
+    discord_path = os.path.join(funcs.DISCORD_PARENT_PATH, discord_core_folder)
+    is_discord_up_to_date = discord_core_folder == funcs.LAST_INSTALLED_DISCORD_VERSION and not funcs.DISABLE_DISCORD_VERSION_CHECKING
 
-        if discord_core_folder == funcs.LAST_INSTALLED_DISCORD_VERSION and not funcs.DISABLE_DISCORD_VERSION_CHECKING:
-            
-            logger.info("Discord is up to date.")
-
+    if is_discord_up_to_date:
+        logger.info("Discord is up to date.")
+    else:
         funcs.LAST_INSTALLED_DISCORD_VERSION = discord_core_folder
         dump_settings()
-    except FileNotFoundError as e:
-        logger.error(str(e))
-        sys.exit(1)
 
-    logger.info("Installing BetterDiscord...")
-    install_betterdiscord(discord_path)
+    is_betterdiscord_up_to_date = not check_for_betterdiscord_updates()
+    if not is_discord_up_to_date or not is_betterdiscord_up_to_date:
+        if not is_discord_up_to_date:
+            logger.info("Discord was updated.")
 
-    PLUGIN_URLS = [
-        'https://raw.githubusercontent.com/riolubruh/YABDP4Nitro/main/YABDP4Nitro.plugin.js',
-        'https://mwittrien.github.io/BetterDiscordAddons/Plugins/PluginRepo/PluginRepo.plugin.js'
-    ]
-    PLUGIN_SAVE_PATHS = [
-        os.path.join(APPDATA, 'BetterDiscord/plugins/YABDP4Nitro.plugin.js'),
-        os.path.join(APPDATA, 'BetterDiscord/plugins/PluginRepo.plugin.js')
-    ]
+        if not is_betterdiscord_up_to_date:
+            logger.info("Found an available update for BetterDiscord.")
 
-    install_plugins(PLUGIN_URLS, PLUGIN_SAVE_PATHS, APPDATA)
+        logger.info("Killing any running Discord processes...")
+        kill_discord()
+        time.sleep(2)
 
-    logger.info("Restarting Discord...")
-    start_discord(funcs.DISCORD_PARENT_PATH)
+        logger.info("Installing BetterDiscord...")
+        install_betterdiscord(discord_path)
+
+        PLUGIN_URLS = [
+            'https://raw.githubusercontent.com/riolubruh/YABDP4Nitro/main/YABDP4Nitro.plugin.js',
+            'https://mwittrien.github.io/BetterDiscordAddons/Plugins/PluginRepo/PluginRepo.plugin.js'
+        ]
+        PLUGIN_SAVE_PATHS = [
+            os.path.join(APPDATA, 'BetterDiscord/plugins/YABDP4Nitro.plugin.js'),
+            os.path.join(APPDATA, 'BetterDiscord/plugins/PluginRepo.plugin.js')
+        ]
+
+        install_plugins(PLUGIN_URLS, PLUGIN_SAVE_PATHS, APPDATA)
+
+        logger.info("Restarting Discord...")
+        start_discord(funcs.DISCORD_PARENT_PATH)
+    else:
+        logger.info("BetterDiscord is up to date.")
 
     logger.info("Installation complete. Exiting in 3 seconds...")
     time.sleep(3)
