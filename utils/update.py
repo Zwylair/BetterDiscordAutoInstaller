6586d88483a4acf9050439acc8f2f691d9f88d6c
Zwylair <82669735+Zwylair@users.noreply.github.com>
2025-04-14 02:14:27 +0300

    feat: improve bdai version checking

--- utils/update.py
+++ utils/update.py
@@ -10,13 +10,37 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='(%(asctime)s) %(message)s')
 
 
+def is_version_greater(first_version: str, second_version: str) -> bool:
+    first = first_version.split(".")
+    second = second_version.split(".")
+
+    try:
+        first = list(map(lambda i: int(i), first))
+        second = list(map(lambda i: int(i), second))
+    except ValueError:
+        print("One of the versions does not match the n.n.n-like version template")
+        return False
+
+    if len(first) > len(second):
+        second += [0] * (len(first) - len(second))
+    elif len(second) > len(first):
+        first += [0] * (len(second) - len(first))
+
+    for first_version_number, second_version_number in zip(first, second):
+        if first_version_number > second_version_number:
+            return True
+        elif second_version_number > first_version_number:
+            return False
+    return False
+
+
 def check_for_bdai_updates() -> bool:
     """Checks for updates and return True if there is an available update, False otherwise"""
 
     latest_release_url = requests.head(config.BDAI_LATEST_RELEASE_PAGE_URL, allow_redirects=True)
     latest_available_version = latest_release_url.url.split('/')[-1].lstrip('v')
 
-    if config.BDAI_SCRIPT_VERSION != latest_available_version:
+    if is_version_greater(latest_available_version, config.BDAI_SCRIPT_VERSION):
         logger.info(f'A new version available ({config.BDAI_SCRIPT_VERSION} -> {latest_available_version}).')
 
         if config.DISABLE_BDAI_AUTOUPDATE:
